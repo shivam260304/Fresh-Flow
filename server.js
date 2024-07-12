4bf1ee6500d436d79c66f159f1b70ea748d26f5d
shivam260304 <shivam260304@gmail.com>
2024-07-12 18:51:13 +0530

    second

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(expressLayout);
+app.use(express.static('public'));
+// app.use(expressLayout);
+app.set('views', './resources/views');
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/resources/views'));
 
 app.get("/",(req,res)=>{
-    res.render("home");
-})
+    res.render("home.ejs");
+});
 
 app.listen(3000,()=>{
     console.log("FreshFlow");
