25354453e9e1a37bb23b96d982937bc907e5de65
shivam260304 <shivam260304@gmail.com>
2024-09-11 11:59:39 +0530

    imagesUpdated

--- app/models/menu.js
+++ app/models/menu.js
@@ -4,6 +4,7 @@ let Schema = mongoose.Schema;
 
 const menuSchema = new Schema({
     name: {type:String, required:true},
+    description: {type:String, required:true},
     image: {type:String, required:true},
     price: {type:Number, required:true},
     size: {type:String, required:true},
